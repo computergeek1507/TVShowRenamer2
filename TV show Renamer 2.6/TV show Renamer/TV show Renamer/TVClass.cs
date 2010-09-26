65647444c7f4a2a0beac9b95b1a179a07ab0cbe1
scooter_seh_cp <SND\scooter_seh_cp>
2010-09-26 02:22:41 +0000

    add TVClass to store data about each file that is added. limited folder browser to only add 500 files due to performance issues (hope to fix it)

---
+++ TV show Renamer 2.6/TV show Renamer/TV show Renamer/TVClass.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TV_show_Renamer
+{
+    public class TVClass
+    {
+        string _fileFolder;//origonal folder
+        string _fileName;//origonal file name
+        string _fileExtention;//origonal file Extention
+        string _fileTitle="";//Title files        
+        string _newFileName;//new file name
+
+        public TVClass(string fileFolder, string fileName, string fileExtention)
+        {
+            _fileFolder = fileFolder;
+            _fileName = _newFileName = fileName;
+            _fileExtention = fileExtention;
+        }
+
+        public string FileName
+        {
+            get { return _fileName; }
+            set { _fileName = value; }
+        }
+
+        public string NewFileName
+        {
+            get { return _newFileName; }
+            set { _newFileName = value; }
+        }
+
+        public string FileFolder
+        {
+            get { return _fileFolder; }
+            set { _fileFolder = value; }
+        }
+        public string FileExtention
+        {
+            get { return _fileExtention; }
+            set { _fileExtention = value; }
+        }
+        public string FileTitle
+        {
+            get { return _fileTitle; }
+            set { _fileTitle = value; }
+        }
+        public string FullFileName
+        {
+            get { return _fileFolder +"//"+ _fileName; }
+            
+        }
+        public string NewFullFileName
+        {
+            get { return _fileFolder + "//" + _newFileName; }
+
+        }
+    }
+}
