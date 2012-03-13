f1639916c224204b19abe431710ff464c01b425e
Scott H <computergeek1507@users.noreply.github.com>
2012-03-13 04:47:32 +0000

    Working on new features

---
+++ TV show Renamer/EPGuides.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TV_Show_Renamer
+{
+    class EPGuides
+    {
+        string folder = null;
+        
+        public EPGuides(string newFolder)
+        {
+            folder = newFolder;            
+        }
+        public int findTitle(string ShowName)
+        {
+            return -1;
+        }
+
+        public string getTitle(int seriesID, int season, int episode)
+        { 
+            return "";        
+        }
+    }
+}
